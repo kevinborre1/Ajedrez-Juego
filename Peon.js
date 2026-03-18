27904a958d68ccc02111131ae08139598babfa96
Kevin Borre <kevinborreboca@gmail.com>
2026-03-18 20:43:21 -0300

    agregando la clase peon

---
+++ Peon.js
@@ -0,0 +1,14 @@
+class Peon extends Pieza {
+    
+    constructor(color) {
+        super(color);
+    }
+
+    if (puedeMover(origen, destino, tablero) == true) {
+        return esHorizontal(destino);
+
+    }   
+
+
+}
+
